75074fdad7eb8d0bdf467a2dd743a6409395069b
vy3191 <venkyafhs@gmail.com>
2020-03-04 18:32:42 -0500

    Added knexfile

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: '/data/car-dealer.db3'
+    }
+  },
+
+ 
+
+};
