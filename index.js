386b5060ee29a41f95eda17a47859a86bb5d6d2f
vy3191 <venkyafhs@gmail.com>
2020-03-04 18:22:55 -0500

    Basic structure on index.js done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-cons
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 8080;
+const server = express();
+
+server.use(helmet());
+server.use(morgan('tiny'));
+server.use(express.json());
+
+server.use((req,res,next) => res.status(404).json({msg:'Route Not Found'}));
+server.use((err,req,res,next) => res.status(500).json({msg:'Something wrong with server'}));
+
+server.listen(PORT,(req,res) => {
+   console.log(`App is up and running at http://localhost:${PORT}`);
+})
